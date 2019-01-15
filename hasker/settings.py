c64da64386706f56aef8a3fdb17857fadeca6ac1
ivangorban <ivan@igorban.ru>
2019-01-15 16:14:46 +0300

    set sqlite for test

--- hasker/settings.py
+++ hasker/settings.py
@@ -83,19 +83,19 @@ WSGI_APPLICATION = 'hasker.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "hasker",
-        "USER": "hasker",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #     "NAME": "hasker",
+    #     "USER": "hasker",
+    #     "PASSWORD": "",
+    #     "HOST": "localhost",
+    #     "PORT": "5432",
+    # }
 }
 
 
